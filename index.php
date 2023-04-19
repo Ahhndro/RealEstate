2c04840b02edb82adc1be9f102db1e1e91347b7e
unknown <Ahhndropocof3@gmail.com>
2023-04-19 20:53:25 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,142 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Real Estate Marketing and Sales of Construction Company</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/solid.min.css" 
+        integrity="sha512-bdTSJB23zykBjGDvyuZUrLhHD0Rfre0jxTd0/jpTbV7sZL8DCth/88aHX0bq2RV8HK3zx5Qj6r2rRU/Otsjk+g==" 
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="head">
+        <nav>
+            <img src="massms/real-estate.png" alt="Massms Logo" class="real-estate-logo" />
+           <div class="logo"></div>
+            <ul>
+
+                <li><a href="#">Home</a></li>
+                <li><a href="#about-me">Properties</a></li>
+                <li><a href="#my-works">Pricing</a></li>         
+                <li><a href="login.html">Appointments</a></li>    
+                <li><a href="register.html">About</a></li>      
+            </ul>
+
+            <a href="login.php">
+              <button class="loginBtn">
+                Login / Sign-up
+              </button> 
+              </a>
+                              
+        </nav>
+        <div class="name-title">
+            <img src="massms/main-model.svg" alt="main-model" class="modelImg " />
+            <div class="main-head">
+              <h1>Real Estate <br> Marketing and Sales</h1>
+              <h4>Real Estate Marketing and Sales of Construction Company <br>
+              </h4><br>
+              <button class="tryBtn2">Try it out for free 
+                <img src= "massms/arrow-right.svg" alt="arrow-right" class="arrow-right"/>
+              </button><br>
+              <div class="yellow-check-container">
+                <h4><img src="massms/yellow-check.svg" alt="yellow-check" class="yellow-check"/>2-minute setup</h4>
+                <h4><img src="massms/yellow-check.svg" alt="yellow-check2" class="yellow-check2"/>No credit card required</h4>
+              </div>
+            </div>
+        </div>
+    </div>
+    <!-- TRY-TRY NA FOOTER -->
+          <footer class="footer">
+            <div class="footer__addr">
+              <h1 class="footer__logo">Ahhndro Devs</h1>
+              <address>
+              <h2>Contact</h2>
+                R. Castillo St. Agdao Davao City
+                <br> 
+                <h2>Email Us</h2>
+                Ahhndropocof3@gmail.com
+              </address>
+            </div>
+            
+            <ul class="footer__nav">
+              <li class="nav__item">
+                <h2 class="nav__title">Media</h2>
+          
+                <ul class="nav__ul">
+                  <li>
+                    <a href="#">Online</a>
+                  </li>
+          
+                  <li>
+                    <a href="#">Print</a>
+                  </li>
+                      
+                  <li>
+                    <a href="#">Alternative Ads</a>
+                  </li>
+                </ul>
+              </li>
+              
+              <li class="nav__item nav__item--extra">
+                <h2 class="nav__title">Technology</h2>
+                
+                <ul class="nav__ul nav__ul--extra">
+                  <li>
+                    <a href="#">Hardware Design</a>
+                  </li>
+                  
+                  <li>
+                    <a href="#">Software Design</a>
+                  </li>
+                  
+                  <li>
+                    <a href="#">Digital Signage</a>
+                  </li>
+                  
+                  <li>
+                    <a href="#">Automation</a>
+                  </li>
+                  
+                  <li>
+                    <a href="#">Artificial Intelligence</a>
+                  </li>
+                  
+                  <li>
+                    <a href="#">IoT</a>
+                  </li>
+                </ul>
+              </li>
+              
+              <li class="nav__item">
+                <h2 class="nav__title">Legal</h2>
+                
+                <ul class="nav__ul">
+                  <li>
+                    <a href="#">Privacy Policy</a>
+                  </li>
+                  
+                  <li>
+                    <a href="#">Terms of Use</a>
+                  </li>
+                  
+                  <li>
+                    <a href="#">Sitemap</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            
+            <div class="legal">
+              <p>&copy; 2019 Something. All rights reserved.</p>
+              
+              <div class="legal__links">
+                <span>Made with <span class="heart">Ã¢â„¢Â¥</span> remotely from Anywhere</span>
+              </div>
+            </div>
+          </footer>
+      </div>
+</body>
+</html>
